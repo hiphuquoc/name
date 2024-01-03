3bff9a804dbce593c84cbb4118ded7f13462fd3b
root <root@DESKTOP-3JVPJ2G.localdomain>
2024-01-03 09:21:46 +0700

    --amend

--- resources/views/wallpaper/schema/imageObject.blade.php
+++ resources/views/wallpaper/schema/imageObject.blade.php
@@ -3,17 +3,19 @@
         $xhtml              = null;
         $i                  = 1;
         foreach($data as $d){
-            if($i!=1) $xhtml .= ', ';
-            $urlImage        = config('main.google_cloud_storage.default_domain').$wallpaper->infoWallpaper->file_cloud_wallpaperle_url_hosting;
-            $name           = $d->name ?? null;
-            $description    = $d->description ?? $d->name;
-            $xhtml          .= '{
-                                "@type": "ImageObject",
-                                "contentUrl": "'.$urlImage.'",
-                                "name": "'.$name.'",
-                                "description": "'.$description.'"
-                            }';
-            ++$i;
+            if(!empty($d)){
+                if($i!=1) $xhtml .= ', ';
+                $urlImage        = config('main.google_cloud_storage.default_domain').$wallpaper->infoWallpaper->file_cloud_wallpaperle_url_hosting;
+                $name           = $d->name ?? null;
+                $description    = $d->description ?? $d->name;
+                $xhtml          .= '{
+                                    "@type": "ImageObject",
+                                    "contentUrl": "'.$urlImage.'",
+                                    "name": "'.$name.'",
+                                    "description": "'.$description.'"
+                                }';
+                ++$i;
+            }
         }
     @endphp
     <script type="application/ld+json">
