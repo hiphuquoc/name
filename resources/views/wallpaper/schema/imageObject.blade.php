1b9b2f0682b1914f78d4adda85da6f719c0dfe03
root <root@DESKTOP-3JVPJ2G.localdomain>
2024-01-03 09:18:14 +0700

    --amend

--- resources/views/wallpaper/schema/imageObject.blade.php
+++ resources/views/wallpaper/schema/imageObject.blade.php
@@ -5,11 +5,7 @@
         foreach($data as $d){
             if($i!=1) $xhtml .= ', ';
             $urlImage        = config('main.google_cloud_storage.default_domain').$wallpaper->infoWallpaper->file_cloud_wallpaperle_url_hosting;
-            // if(empty($language)||$language=='vi'){
-            //     $name           = $d->name;
-            //     $description    = $d->description ?? $d->name;
-            // }
-            $name           = $d->name;
+            $name           = $d->name ?? null;
             $description    = $d->description ?? $d->name;
             $xhtml          .= '{
                                 "@type": "ImageObject",
