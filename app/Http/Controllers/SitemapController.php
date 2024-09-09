2ba5de6c0a4f76be073cc584f422130d2582ef61
root <root@DESKTOP-3JVPJ2G.localdomain>
2024-09-09 13:28:52 +0700

    fix sitemap upcode name

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 // use Illuminate\Http\Request;
 // use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use App\Helpers\Image;
 
 use App\Http\Controllers\Admin\HelperController;
 use App\Models\Seo;
@@ -75,13 +77,15 @@ class SitemapController extends Controller {
                     foreach($item->seos as $seo){
                         if(!empty($seo->infoSeo)&&$seo->infoSeo->language==$language){
                             $url            = env('APP_URL').'/'.self::replaceSpecialCharactorXml($seo->infoSeo->slug_full);
+                            $urlImage       = env('APP_URL').Storage::url(config('main_name.logo_main'));
+                            if(!empty($item->seo->image)) $urlImage   = Image::getUrlImageLargeByUrlImage($item->seo->image);
                             $sitemapXhtml   .= '<url>
                                                     <loc>'.$url.'</loc>
                                                     <lastmod>'.date('c', strtotime($seo->infoSeo->updated_at)).'</lastmod>
                                                     <changefreq>hourly</changefreq>
                                                     <priority>1</priority>
                                                     <image:image>
-                                                        <image:loc>'.env('APP_URL').'/'.$seo->infoSeo->image.'</image:loc>
+                                                        <image:loc>'.$urlImage.'</image:loc>
                                                         <image:title>'.self::replaceSpecialCharactorXml($seo->infoSeo->seo_title).'</image:title>
                                                     </image:image>
                                                 </url>';
