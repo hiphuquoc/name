4ffb0e4e4aa27ffd222db35578b858fb84710298
root <root@DESKTOP-3JVPJ2G.localdomain>
2025-01-03 11:41:32 +0700

    turn on request translate page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -80,20 +80,20 @@ class HomeController extends Controller {
     public static function test(Request $request){
 
         // tạo ngôn ngữ còn thiếu hàng loạt 
-        // $categories = Category::select('*')
-        //                 ->with('seo', 'seos')
-        //                 ->get();
-        // foreach($categories as $infoC) TranslateController::createJobTranslateAndCreatePage($infoC);
+        $categories = Category::select('*')
+                        ->with('seo', 'seos')
+                        ->get();
+        foreach($categories as $infoC) TranslateController::createJobTranslateAndCreatePage($infoC);
 
-        // $tags       = Tag::select('*')
-        //                 ->with('seo', 'seos')
-        //                 ->get();
-        // foreach($tags as $infoT) TranslateController::createJobTranslateAndCreatePage($infoT);
+        $tags       = Tag::select('*')
+                        ->with('seo', 'seos')
+                        ->get();
+        foreach($tags as $infoT) TranslateController::createJobTranslateAndCreatePage($infoT);
 
-        // $products   = Product::select('*')
-        //                 ->with('seo', 'seos')
-        //                 ->get();
-        // foreach($products as $infoP) TranslateController::createJobTranslateAndCreatePage($infoP);
+        $products   = Product::select('*')
+                        ->with('seo', 'seos')
+                        ->get();
+        foreach($products as $infoP) TranslateController::createJobTranslateAndCreatePage($infoP);
         
     }
 
