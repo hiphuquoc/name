83f99f939223bc8b61db862b2de697388edb82c0
root <root@DESKTOP-3JVPJ2G.localdomain>
2025-01-22 19:50:35 +0700

    fix translate by 1 box content

--- app/Http/Controllers/Admin/TranslateController.php
+++ app/Http/Controllers/Admin/TranslateController.php
@@ -168,12 +168,11 @@ class TranslateController extends Controller {
             }
         }
         if (!empty($idSeo)&&$language!='vi') {
-            // /* kiểm tra xem có phải đang chạy có bất kì row status = 0 */
-            // $infoFlag   = JobAutoTranslate::select('*')
-            //     ->where('seo_id', $idSeo)
-            //     ->where('status', 0)
-            //     ->first();
-                $infoFlag = '';
+            /* kiểm tra xem có phải đang chạy có bất kì row status = 0 */
+            $infoFlag   = JobAutoTranslate::select('*')
+                ->where('seo_id', $idSeo)
+                ->where('status', 0)
+                ->first();
             if (empty($infoFlag)) {
                 /* lấy content bảng tiếng việt */
                 $contents   = SeoContent::select('*')
