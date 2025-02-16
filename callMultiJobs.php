dd4947125364fe681db4620b095648ecaf6b7e47
root <root@DESKTOP-3JVPJ2G.localdomain>
2025-02-16 13:17:21 +0700

    work for job multi ordering

--- callMultiJobs.php
+++ callMultiJobs.php
@@ -19,9 +19,10 @@ while ($i < 2) { // Chạy tối đa 2 lần
         $jobsToDispatch = $jobPerTime - $runningJobsCount;
 
         // Lấy các job chưa được reserve (reserved_at IS NULL) theo số lượng cần thiết
+        $ordering = $i==0 ? 'asc' : 'desc';
         $jobs = DB::table('jobs')
             ->whereNull('reserved_at')
-            ->orderBy('id', 'asc')
+            ->orderBy('id', $ordering)
             ->limit($jobsToDispatch)
             ->get();
 
