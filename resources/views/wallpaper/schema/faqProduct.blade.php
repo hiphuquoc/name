bb3953128ac58e004a3e4284c62f38b451f6fad5
root <root@DESKTOP-3JVPJ2G.localdomain>
2025-04-25 01:06:21 +0700

    fix schema FAQ

--- resources/views/wallpaper/schema/faqProduct.blade.php
+++ resources/views/wallpaper/schema/faqProduct.blade.php
@@ -1,20 +1,20 @@
 @if(!empty($dataFaq))
     <script type="application/ld+json">
+{
+    "@context": "https://schema.org",
+    "@type": "FAQPage",
+    "mainEntity": [
+        @foreach($dataFaq as $index => $faq)
         {
-            "@context": "https://schema.org",
-            "@type": "FAQPage",
-            "mainEntity": [
-                @foreach($dataFaq as $index => $faq)
-                    {
-                        "@type": "Question",
-                        "name": "{{ addslashes($faq['question']) }}",
-                        "acceptedAnswer": {
-                            "@type": "Answer",
-                            "text": "{{ addslashes($faq['answer']) }}"
-                        }
-                    }@if(!$loop->last),@endif
-                @endforeach
-            ]
-        }
+            "@type": "Question",
+            "name": "{{ addslashes($faq['question']) }}",
+            "acceptedAnswer": {
+                "@type": "Answer",
+                "text": "{!! addslashes($faq['answer']) !!}"
+            }
+        }@if(!$loop->last),@endif
+        @endforeach
+    ]
+}
     </script>
 @endif
