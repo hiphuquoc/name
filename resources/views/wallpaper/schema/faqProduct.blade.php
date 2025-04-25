55163209993295259abc4e49ee214687b2922e9b
root <root@DESKTOP-3JVPJ2G.localdomain>
2025-04-26 01:38:11 +0700

    fix config faq

--- resources/views/wallpaper/schema/faqProduct.blade.php
+++ resources/views/wallpaper/schema/faqProduct.blade.php
@@ -1,20 +1,20 @@
 @if(!empty($dataFaq))
     <script type="application/ld+json">
-{
-    "@context": "https://schema.org",
-    "@type": "FAQPage",
-    "mainEntity": [
-        @foreach($dataFaq as $index => $faq)
         {
-            "@type": "Question",
-            "name": "{{ addslashes($faq['question']) }}",
-            "acceptedAnswer": {
-                "@type": "Answer",
-                "text": "{{ addslashes($faq['answer']) }}"
-            }
-        }@if(!$loop->last),@endif
-        @endforeach
-    ]
-}
+            "@context": "https://schema.org",
+            "@type": "FAQPage",
+            "mainEntity": [
+                @foreach($dataFaq as $index => $faq)
+                    {
+                        "@type": "Question",
+                        "name": {!! json_encode($faq['question'], JSON_UNESCAPED_UNICODE) !!},
+                        "acceptedAnswer": {
+                            "@type": "Answer",
+                            "text": {!! json_encode($faq['answer'], JSON_UNESCAPED_UNICODE) !!}
+                        }
+                    }@if(!$loop->last),@endif
+                @endforeach
+            ]
+        }
     </script>
 @endif
